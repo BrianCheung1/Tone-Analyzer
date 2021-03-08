2eeb87ade74e12d11e6d9f009a4bb38593ac1325
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2021-03-08 16:52:57 -0500

    test

--- IBMWattson.py
+++ IBMWattson.py
@@ -2,68 +2,74 @@ import json
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-#Api Access
-#Using latest version
-authenticator = IAMAuthenticator('')
+import randomtest
+
+# Api Access
+# Using latest version
+authenticator = IAMAuthenticator(
+    'cfWZ9e9Dqi0mjjj6AfVqHo6reWDVCVZF9is5_pwEPMLv')
 tone_analyzer = ToneAnalyzerV3(
     version='2017-09-21',
     authenticator=authenticator
 )
-#url corresponds to location closest
-tone_analyzer.set_service_url('https://api.us-east.tone-analyzer.watson.cloud.ibm.com/instances/02ad5ad4-e149-4347-a67e-b94550f58e84')
+# url corresponds to location closest
+tone_analyzer.set_service_url(
+    'https://api.us-east.tone-analyzer.watson.cloud.ibm.com/instances/02ad5ad4-e149-4347-a67e-b94550f58e84')
 
 
-text = ""
-with open("Speech.txt", encoding="utf8") as f:
-    for line in f:
-        text += line
+#return jsonfile of text
+def text_to_analyze(text):
+    # json file from api
+    tone_analysis = tone_analyzer.tone(
+        {'text': text},
+        content_type='application/json'
+    ).get_result()
+
+    return tone_analysis
+
+# prints the tone score for the entire document
+# scores range from 0.5-1.0
+# scores under 0.0 will result in empty array
+# scores above .75 are high accuracy
+def tone_of_document(text):
+    document_tone = "Document Tone\n"
+    for elements in text_to_analyze(text)["document_tone"]["tones"]:
+        document_tone +=("Tone Name: " + str(elements["tone_name"]) + "\n")
+        document_tone +=("Score: " + str(elements["score"]) +
+              " Percent: " + str("{0:.0%}".format(elements["score"])) + "\n")
+
+        if(elements["score"] > 0.75):
+            document_tone +=("Accurate? : Yes\n")
+        else:
+            document_tone +=("Accurate? : No\n")
+    return document_tone
+# prints the tone score for each setence in the document
+# scores range from 0.5-1.0
+# scores under 0.0 will result in empty array
+# scores above .75 are high accuracy
 
 
-#json file from api
-tone_analysis = tone_analyzer.tone(
-    {'text': text},
-    content_type='application/json'
-).get_result()
+def tone_of_sentences(text):
+    sentence_tone = "Sentence Tones\n"
+    for index, elements in enumerate(text_to_analyze(text)["sentences_tone"], start=1):
+        sentence_tone +=(f"Sentence Number: {index}\n")
+        sentence_tone +=("Sentence: " + elements["text"] + "\n")
+        if not elements["tones"]:
+            sentence_tone +=("Accurate? : Not Enough Data\n")
+        else:
+            for tones in elements["tones"]:
+                sentence_tone +=("Tone Name: " + tones["tone_name"] + "\n")
+                sentence_tone +=("Score: " + str(tones["score"]) +
+                      " Percent: " + str("{0:.0%}".format(tones["score"])) + "\n")
+                if(tones["score"] > 0.75):
+                    sentence_tone +=("Accurate? : Yes\n")
+                else:
+                    sentence_tone +=("Accurate? : No\n")
+        sentence_tone += "\n"
+    return sentence_tone
 
-#prints entire json file in a neat format
-#print(json.dumps(tone_analysis, indent=2))
-
-document_tone = tone_analysis["document_tone"]["tones"]
-sentences_tone = tone_analysis["sentences_tone"]
-
-#prints the tone score for the entire document
-#scores range from 0.5-1.0
-#scores under 0.0 will result in empty array
-#scores above .75 are high accuracy
-print("Document Tones\n")
-for elements in document_tone:
-    print("Tone Name: " + str(elements["tone_name"]))
-    print("Score: " + str(elements["score"]) +" Percent: "+ str("{0:.0%}".format(elements["score"])))
-
-    if(elements["score"] > 0.75):
-        print("Accurate? : Yes")
-    else:
-        print("Accurate? : No")
-    print("")
-  
-#prints the tone score for each setence in the document
-#scores range from 0.5-1.0
-#scores under 0.0 will result in empty array
-#scores above .75 are high accuracy
-print("Sentence Tones\n")
-for index, elements in enumerate(sentences_tone, start=1):
-    print(f"Sentence Number: {index}")
-    print("Sentence: "+ elements["text"])
-    if not elements["tones"]:
-        print("Accurate? : Not Enough Data")
-    else:
-        for tones in elements["tones"]:
-            print("Tone Name: " + tones["tone_name"])
-            print("Score: " + str(tones["score"]) + " Percent: "+ str("{0:.0%}".format(tones["score"])))
-            if(tones["score"] > 0.75):
-                print("Accurate? : Yes")
-            else:
-                print("Accurate? : No")
-    print("")
-
-
+if __name__ == '__main__':
+   
+    text_to_analyze(tone_of_sentences(randomtest.millerscrape()[0]['transcript']))
+    print(tone_of_document(randomtest.millerscrape()[0]['transcript']))
+    print(tone_of_sentences(randomtest.millerscrape()[0]['transcript']))
